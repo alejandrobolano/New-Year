f1dc25117fe05bc1468673dea202d94fae870880
Alejandro Bolaño <elbola.cuba1992@gmail.com>
2024-12-25 12:23:42 +0100

    feat: Update github code link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer : React.FC = () => {
     <div className="text-center">
       <div className="flex justify-center items-center gap-4 mb-8 text-xs text-gray-600">
        <a href="https://alejandrobolano.vercel.app" target="_blank">Copyright © 2024 😍 All rights reserved.</a>
-        <a href="https://github.com/alejandrobolano/Merry-Xmas" target="_blank">💻 Code</a>
+        <a href="https://github.com/alejandrobolano/New-Year" target="_blank">💻 Code</a>
         <a href="/readme">🤔 Readme</a>
       </div>
     </div>
